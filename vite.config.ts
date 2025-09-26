70f2f09db829f49559f2a275065c9b10dbdc42fd
pierre.goemans <pierre.goemans.ext@melting-point.fr>
2025-09-26 14:36:36 +0200

    refactor: standardize formatting in workflow and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/mac-woop/', // Remplacez 'mac-woop' par le nom de votre repository GitHub
-})
+  base: "/mac-woop/", // Remplacez 'mac-woop' par le nom de votre repository GitHub
+});
